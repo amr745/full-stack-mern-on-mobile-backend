64db9d55ef3b1681dc3f7db2772b248c717a7c17
Ashley Richard <ashleymrichard@gmail.com>
2022-06-03 04:03:25 -0700

    add home route image

--- server.js
+++ server.js
@@ -26,12 +26,13 @@ app.use(express.json()) // parse json bodies
 const Appointments = require("./models/AppointmentsSchema")
 const barbers = require("./barbers.json");
 const services = require("./services.json");
+const home = require("./home.json");
 
 // ROUTES
 
-// TEST ROUTE
+// HOME ROUTE
 app.get("/", (req, res) => {
-  res.send("hello world")
+  res.json(home);
 })
 
 // BARBERS ROUTE
